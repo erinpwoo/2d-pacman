227b885bc834134f37b98fb526a35551e35e7367
Erin <erinpwoo@yahoo.com>
2020-10-17 14:56:27 -0700

    Score update with pellets

---
+++ Assets/Scripts/Pacdot.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pacdot : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject pacman;
+    
+    void Start()
+    {
+        pacman = GameObject.FindWithTag("Pacman");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+       if (collision.CompareTag("Pacman"))
+       {
+           if (gameObject.CompareTag("Pacdot"))
+           {
+               pacman.GetComponent<Pacman>().score += 10;
+           } else if (gameObject.CompareTag("Pacdot"))
+           {
+               pacman.GetComponent<Pacman>().score += 50;
+           }
+           gameObject.SetActive(false);
+       }
+    }
+}
--- Assets/Scripts/Pacman.cs
+++ Assets/Scripts/Pacman.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Pacman : MonoBehaviour
 {
     public float speed;
     private Rigidbody2D rb;
     private Vector2 direction = Vector2.zero; // Pacman continues moving in direction of last input
+
+    public int score;
+    public Text scoreText;
 
     // Start is called before the first frame update
     void Start()
@@ -19,27 +23,35 @@ public class Pacman : MonoBehaviour
     void Update()
     {
         Move();
-        if (Input.GetKey(KeyCode.UpArrow)) {
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.I)) {
             direction = Vector2.up;
             transform.localScale = new Vector3(2.9f, 2.9f, 2.9f);
             transform.localRotation = Quaternion.Euler(0, 0, 90);
-        } else if (Input.GetKey(KeyCode.DownArrow))
+        } else if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.M))
         {
             direction = Vector2.down;
             transform.localScale = new Vector3(2.9f, 2.9f, 2.9f);
             transform.localRotation = Quaternion.Euler(0, 0, 270);
         }
-        else if (Input.GetKey(KeyCode.LeftArrow))
+        else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.J))
         {
-           direction = Vector2.left;
-           transform.localScale = new Vector3(-2.9f, 2.9f, 2.9f);
+            direction = Vector2.left;
+            transform.localScale = new Vector3(-2.9f, 2.9f, 2.9f);
             transform.localRotation = Quaternion.Euler(0, 0, 0);
-        } else if (Input.GetKey(KeyCode.RightArrow))
+        } else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.K))
         {
             direction = Vector2.right;
             transform.localScale = new Vector3(2.9f, 2.9f, 2.9f);
             transform.localRotation = Quaternion.Euler(0, 0, 0);
         }
+        if (score < 100)
+        {
+            scoreText.text = score.ToString("d2");
+        } else
+        {
+            scoreText.text = score.ToString();
+        }
+        
     }
 
     void Move()
